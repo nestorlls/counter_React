edaef57b7b19548d2e9d2e36179a9fa6ce67a078
Paul Llanque <llanquepol@gmail.com>
2023-02-25 23:57:53 -0500

    add events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import fccLogo from './images/freecodecamp-logo.png';
 import './App.css';
+import Button from './components/button';
 
 function App() {
+  const listenClick = () => {
+    console.log('clickkkk');
+  };
+
+  const resetCounter = () => {
+    console.log('resetttttttt');
+  };
   return (
     <div className='App'>
       <div className='fcc-logo-container'>
         <img src={fccLogo} alt='logo fcc' className='fcc-logo' />
       </div>
-      <div className="container">
-        
+      <div className='container'>
+        <Button text='click' isButtonClick={true} listenClick={listenClick} />
+        <Button text='reset' isButtonClick={false} listenClick={resetCounter} />
       </div>
     </div>
   );
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../sytesheets/Button.css'
 
 function Button({ text, isButtonClick, listenClick }) {
   return (
