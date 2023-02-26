5b8ef49dbba31ecb5c09f3b9630638cb2e5a5599
Paul Llanque <llanquepol@gmail.com>
2023-02-26 00:22:03 -0500

    not necesary import React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../sytesheets/Counter.css';
 
 function Counter({ numClick }) {
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../sytesheets/Button.css'
 
 function Button({ text, isButtonClick, listenClick }) {
